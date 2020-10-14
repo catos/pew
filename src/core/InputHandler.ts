a07c219a560323f125f2ec048212366afecd2dcd
Cato Skogholt <cskogholt@gmail.com>
2020-10-14 17:09:42 +0200

    mouse is back

--- src/core/InputHandler.ts
+++ src/core/InputHandler.ts
@@ -1,6 +1,6 @@
-import Vector2 from '../lib/Vector2.js'
-import Game from '../Game.js'
-import TransformComponent from '../scene/components/TransformComponent.js'
+import Vector2 from "../lib/Vector2.js"
+import Game from "../Game.js"
+import TransformComponent from "../scene/components/TransformComponent.js"
 
 export interface IPewEvent extends UIEvent {
   keysDown: Map<string, boolean>
@@ -13,77 +13,80 @@ export default class InputHandler {
   game: Game
   keysDown: Map<string, boolean>
   mappedKeys: string[]
-  // position: Vector2
+  position: Vector2
 
   constructor(game: Game) {
     this.game = game
     this.keysDown = new Map()
-    this.mappedKeys = ['F1', 'F2', 'F3', 'F4', 'F5']
+    this.mappedKeys = ["F1", "F2", "F3", "F4", "F5"]
+    this.position = Vector2.ZERO
   }
 
   listenTo = (window: Window, callback: (event: IPewEvent) => void) => {
     // TODO: move to separate MouseInputHandler ?
     // , 'mousedown', 'mouseup', 'mousemove'
-    ['keydown', 'keyup'].forEach(eventName => {
-      window.addEventListener(eventName, (event: IPewEvent) => {
+    ;["keydown", "keyup", "mousedown", "mouseup", "mousemove"].forEach(
+      (eventName) => {
+        window.addEventListener(eventName, (event: IPewEvent) => {
+          if (event instanceof KeyboardEvent) {
+            const { code, type } = event
 
-        if (event instanceof KeyboardEvent) {
-          const { code, type } = event
+            // TODO: only handle configured keys ?
+            // if (!this.keyMap.has(code)) {
+            //   return
+            // }
 
-          // TODO: only handle configured keys ?
-          // if (!this.keyMap.has(code)) {
-          //   return
-          // }
+            // Prevent default behaviour on all configured keys
+            if (this.mappedKeys.find((p) => p === code)) {
+              event.preventDefault()
+            }
 
-          // Prevent default behaviour on all configured keys
-          if (this.mappedKeys.find(p => p === code)) {
-            event.preventDefault()
+            // Prevent multifire
+            if (this.keysDown.has(code) && type === "keydown") {
+              return
+            }
+
+            // Update keysdown
+            if (type === "keydown") {
+              this.keysDown.set(code, true)
+            } else if (type === "keyup") {
+              this.keysDown.delete(code)
+            }
           }
 
-          // Prevent multifire
-          if (this.keysDown.has(code) && type === 'keydown') {
-            return
-          }
-
-          // Update keysdown
-          if (type === 'keydown') {
-            this.keysDown.set(code, true)
-          } else if (type === 'keyup') {
-            this.keysDown.delete(code)
-          }
-        }
-
-        // TODO: move to separate MouseInputHandler ?
-        // if (event instanceof MouseEvent) {
-        //   this.position = this.getEventPosition(event)
-        // }
-        // event.position = this.position
-
-        event.keysDown = this.keysDown
-        event.isMousePressed = event.type === 'mousedown'
-        event.isKeyPressed = this.isKeyPressed(event)
-
-        callback(event)
-      })
-    })
-
-  }
-
-  getEventPosition = (event: MouseEvent) => {
-    const { canvas: { canvasRect } } = this.game
-    const cameraTransform = this.game.scene.camera.getComponent<TransformComponent>('transform')
-
-    return new Vector2(
-      event.clientX - Math.floor(canvasRect.left) + cameraTransform.position.x,
-      event.clientY - Math.floor(canvasRect.top) + window.scrollY + cameraTransform.position.y
+          event.position =
+            event instanceof MouseEvent
+              ? this.getEventPosition(event)
+              : Vector2.ZERO
+          event.isMousePressed = event.type === "mousedown"
+          event.keysDown = this.keysDown
+          event.isKeyPressed = this.isKeyPressed(event)
+          callback(event)
+        })
+      }
     )
   }
 
-  isKeyPressed = (event: UIEvent) =>
-    (key: string) => {
-      const result = event.type === 'keydown' && this.keysDown.has(key)
-      // console.log(event.type, key, result)
-      return result
-    }
+  getEventPosition = (event: MouseEvent) => {
+    const {
+      canvas: { canvasRect },
+    } = this.game
+    const cameraTransform = this.game.scene.camera.getComponent<
+      TransformComponent
+    >("transform")
 
+    return new Vector2(
+      event.clientX - Math.floor(canvasRect.left) + cameraTransform.position.x,
+      event.clientY -
+        Math.floor(canvasRect.top) +
+        window.scrollY +
+        cameraTransform.position.y
+    )
+  }
+
+  isKeyPressed = (event: UIEvent) => (key: string) => {
+    const result = event.type === "keydown" && this.keysDown.has(key)
+    // console.log(event.type, key, result)
+    return result
+  }
 }
--- src/scene/systems/DebugSystem.ts
+++ src/scene/systems/DebugSystem.ts
@@ -1,18 +1,18 @@
-import System from './System.js'
-import TransformComponent from '../components/TransformComponent.js'
-import JumpComponent from '../components/JumpComponent.js'
-import ClimbComponent from '../components/ClimbComponent.js'
-import HitboxComponent from '../components/HitboxComponent.js'
-import MovementComponent from '../components/MovementComponent.js'
-import CrouchComponent from '../components/CrouchComponent.js'
-import DashComponent from '../components/DashComponent.js'
-import Entity from '../entities/Entity.js'
-import HitpointsComponent from '../components/HitpointsComponent.js'
-import Vector2 from '../../lib/Vector2.js'
-import { IPewEvent } from '../../core/InputHandler.js'
+import System from "./System.js"
+import TransformComponent from "../components/TransformComponent.js"
+import JumpComponent from "../components/JumpComponent.js"
+import ClimbComponent from "../components/ClimbComponent.js"
+import HitboxComponent from "../components/HitboxComponent.js"
+import MovementComponent from "../components/MovementComponent.js"
+import CrouchComponent from "../components/CrouchComponent.js"
+import DashComponent from "../components/DashComponent.js"
+import Entity from "../entities/Entity.js"
+import HitpointsComponent from "../components/HitpointsComponent.js"
+import Vector2 from "../../lib/Vector2.js"
+import { IPewEvent } from "../../core/InputHandler.js"
 
 interface IMenuItem {
-  key: string,
+  key: string
   title: string
 }
 
@@ -25,25 +25,27 @@ export default class DebugSystem extends System {
     this.showMenu = true
     this.showDebug = true
     this.menu = [
-      { key: 'F1', title: 'TGLE' },
-      { key: 'F2', title: 'SHDR' },
-      { key: 'F3', title: 'DEBG' },
-      { key: 'F4', title: 'COLL' },
-      { key: 'F5', title: 'EDIT' },
+      { key: "F1", title: "TGLE" },
+      { key: "F2", title: "SHDR" },
+      { key: "F3", title: "DEBG" },
+      { key: "F4", title: "COLL" },
+      { key: "F5", title: "EDIT" },
     ]
   }
 
   input = (event: IPewEvent) => {
-    if (event.isKeyPressed('F1')) {
+    if (event.isKeyPressed("F1")) {
       this.showMenu = !this.showMenu
     }
-    if (event.isKeyPressed('F3')) {
+    if (event.isKeyPressed("F3")) {
       this.showDebug = !this.showDebug
     }
   }
 
   render = (dt: number) => {
-    const { position: cameraPosition } = this.camera.getComponent<TransformComponent>('transform')
+    const { position: cameraPosition } = this.camera.getComponent<
+      TransformComponent
+    >("transform")
 
     if (this.showMenu) {
       this.drawMenu()
@@ -52,29 +54,28 @@ export default class DebugSystem extends System {
     if (this.showDebug) {
       this.drawDebug()
 
-      this.scene.layers[0].entities.forEach(entity => {
+      this.scene.layers[0].entities.forEach((entity) => {
         this.drawHitpoints(entity, cameraPosition)
         this.drawHitbox(entity, cameraPosition)
       })
     }
   }
 
-
-
   drawHitpoints = (entity: Entity, cameraPosition: Vector2) => {
-    const hitpoints = entity.getComponent<HitpointsComponent>('hitpoints')
+    const hitpoints = entity.getComponent<HitpointsComponent>("hitpoints")
     if (hitpoints) {
-      const { position } = entity.getComponent<TransformComponent>('transform')
+      const { position } = entity.getComponent<TransformComponent>("transform")
       this.game.font.print(
         hitpoints.current.toString(),
         this.context,
         position.x - cameraPosition.x + 2,
-        position.y + 5 - cameraPosition.y)
+        position.y + 5 - cameraPosition.y
+      )
     }
   }
 
   drawHitbox = (entity: Entity, cameraPosition: Vector2) => {
-    const hitbox = entity.getComponent<HitboxComponent>('hitbox')
+    const hitbox = entity.getComponent<HitboxComponent>("hitbox")
     if (hitbox) {
       const { bounds } = hitbox
       this.context.fillStyle = "#0000ff55"
@@ -82,9 +83,9 @@ export default class DebugSystem extends System {
         bounds.left - cameraPosition.x,
         bounds.top - cameraPosition.y,
         bounds.size.x,
-        bounds.size.y)     
+        bounds.size.y
+      )
     }
-
   }
 
   drawMenu = () => {
@@ -92,42 +93,50 @@ export default class DebugSystem extends System {
     const { layers } = this.scene
     const { context } = this
 
-    context.fillStyle = '#000000aa'
+    context.fillStyle = "#000000aa"
     context.fillRect(0, 0, 16 * 32, 13)
 
     let x = 8
-    this.menu.forEach(item => {
+    this.menu.forEach((item) => {
       font.print(`${item.key}:${item.title}`, context, x, 4)
       x += 8 * 7
     })
 
     const objects = layers[0].entities
     font.print(
-      `E:${objects.length} C:${objects.filter(p => p.hasComponents(['hitbox'])).length} FPS:${timer.fps}`,
+      `E:${objects.length} C:${
+        objects.filter((p) => p.hasComponents(["hitbox"])).length
+      } FPS:${timer.fps}`,
       context,
       16 * 25,
-      4)
+      4
+    )
   }
 
   drawDebug = () => {
-    const { font, inputHandler: keyboardHandler, timer } = this.game
+    const { font, inputHandler, timer } = this.game
     const { gravity } = this.scene
 
-    const { position: cameraPosition } = this.camera.getComponent<TransformComponent>('transform')
+    const { position: cameraPosition } = this.camera.getComponent<
+      TransformComponent
+    >("transform")
 
-    const jump = this.player.getComponent<JumpComponent>('jump')
-    const climb = this.player.getComponent<ClimbComponent>('climb')
-    const hitbox = this.player.getComponent<HitboxComponent>('hitbox')
-    const dash = this.player.getComponent<DashComponent>('dash')
-    const movement = this.player.getComponent<MovementComponent>('movement')
-    const transform = this.player.getComponent<TransformComponent>('transform')
-    const crouch = this.player.getComponent<CrouchComponent>('crouch')
-
+    const jump = this.player.getComponent<JumpComponent>("jump")
+    const climb = this.player.getComponent<ClimbComponent>("climb")
+    const hitbox = this.player.getComponent<HitboxComponent>("hitbox")
+    const dash = this.player.getComponent<DashComponent>("dash")
+    const movement = this.player.getComponent<MovementComponent>("movement")
+    const transform = this.player.getComponent<TransformComponent>("transform")
+    const crouch = this.player.getComponent<CrouchComponent>("crouch")
 
     const leftCol = [
       `PLAYER:`,
-      `POSITION: ${transform.position.x.toFixed(2)},${transform.position.y.toFixed(2)}`,
-      `VELOCTY: ${movement.velocity.x.toFixed(2)},${movement.velocity.y.toFixed(2)}`,
+      `POSITION: ${transform.position.x.toFixed(
+        2
+      )},${transform.position.y.toFixed(2)}`,
+      `VELOCTY: ${movement.velocity.x.toFixed(2)},${movement.velocity.y.toFixed(
+        2
+      )}`,
       `DIRECTION: ${movement.direction}, P.HEAD: ${movement.heading}`,
       `DISTANCE: ${movement.distance.toFixed(2)}`,
       `HITBOX: ${hitbox.bounds.size.x}, ${hitbox.bounds.size.y}`,
@@ -135,33 +144,41 @@ export default class DebugSystem extends System {
       `ISCROUCHING: ${crouch.isCrouching}`,
       `ISCLIMBING: ${climb.isClimbing}`,
       `JUMP.CANJUMP: ${jump.canJump}`,
-      `JUMP-TIMERS: ${jump.jumpPressedTimer.toFixed(2)}, ${jump.onGroundTimer.toFixed(2)}`,
+      `JUMP-TIMERS: ${jump.jumpPressedTimer.toFixed(
+        2
+      )}, ${jump.onGroundTimer.toFixed(2)}`,
       `DASH: ${dash.cooldown.toFixed(2)}`,
     ]
-    
-    
+
     font.printArray(leftCol, this.context, 8, 16 * 18)
-    
-    const _keysDown = Array
-      .from(keyboardHandler.keysDown.keys())      
-      .reduce((acc, key) => acc + ' ' + key, '')
+
+    const _keysDown = Array.from(inputHandler.keysDown.keys()).reduce(
+      (acc, key) => acc + " " + key,
+      ""
+    )
 
     const rightCol = [
       `FPS: ${timer.fps.toFixed(2)}`,
-      `TIMER: ${timer.total.toFixed(2)}, ${timer.accumulatedTime.toFixed(2)}, ${timer.lastTime.toFixed(2)}`,
+      `TIMER: ${timer.total.toFixed(2)}, ${timer.accumulatedTime.toFixed(
+        2
+      )}, ${timer.lastTime.toFixed(2)}`,
       `GRAVITY: ${gravity}`,
       `CAM.POS: ${cameraPosition.x.toFixed(2)}, ${cameraPosition.y.toFixed(2)}`,
       `KEYS DOWN: ${_keysDown}`,
     ]
 
     // TODO: needs mouseinputhandler...
-    // if (inputHandler.position) {
-    //   const [x, y] = [Math.floor(inputHandler.position.x), Math.floor(inputHandler.position.y)]
-    //   const [relX, relY] = [Math.floor(x - cameraPosition.x), Math.floor(y - cameraPosition.y)]
-    //   rightCol.push(`MOUSE: ${x}, ${y}`)
-    //   rightCol.push(`MOUSE (RELATIVE): ${relX}, ${relY}`)
-    // }
-    font.printArray(rightCol, this.context, 16 * 16, 16 * 18)
+    const [x, y] = [
+      Math.floor(inputHandler.position.x),
+      Math.floor(inputHandler.position.y),
+    ]
+    const [relX, relY] = [
+      Math.floor(x - cameraPosition.x),
+      Math.floor(y - cameraPosition.y),
+    ]
+    rightCol.push(`MOUSE: ${x}, ${y}`)
+    rightCol.push(`MOUSE (RELATIVE): ${relX}, ${relY}`)
 
+    font.printArray(rightCol, this.context, 16 * 16, 16 * 18)
   }
 }
